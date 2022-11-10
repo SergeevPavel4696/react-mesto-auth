90afa925045a526ea69f26ec7a85f6bea74cb1f5
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-11-10 10:59:27 +0300

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -130,22 +130,19 @@ function App() {
     }
 
     function handleTokenCheck() {
-        if (localStorage.getItem("token")) {
-            const token = localStorage.getItem("token");
-            auth.checkToken(token)
-                .then(response => {
-                    if (response.data) {
-                        setUserEmail(response.data.email);
-                        setLoggedIn(true);
-                        history.push("/");
-                    }
-                })
-                .catch(() => {
-                    setUserEmail("");
-                    setLoggedIn(false);
-                    history.push("/sign-in");
-                });
-        }
+        auth.checkToken()
+            .then(response => {
+                if (response.data) {
+                    setUserEmail(response.data.email);
+                    setLoggedIn(true);
+                    history.push("/");
+                }
+            })
+            .catch(() => {
+                setUserEmail("");
+                setLoggedIn(false);
+                history.push("/sign-in");
+            });
     }
 
     function handleSignText() {
